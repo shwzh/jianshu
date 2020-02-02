728e019aaf6d90b0d907283c903440b87b41b29b
shangwanzhong <3038674015@qq.com>
2020-02-02 09:47:30 +0800

    second commit
    Signed-off-by: shangwanzhong <3038674015@qq.com>

---
+++ src/header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './style'
+
+class header extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+      return (
+          <div>
+
+          </div>
+      )
+    }
+}
+
+export default header
