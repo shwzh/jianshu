75e60d99d201a721b5e0323c6390fd67525b06e2
shangwanzhong <3038674015@qq.com>
2020-02-14 11:18:57 +0800

    feat: 路由1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Header from './header'
 import { IconfontStyle } from "./static/iconfont/iconfont";
 import store from './store'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './home';
+import Detail from './detail';
 
 function App() {
   return (
     <div className="App">
         <IconfontStyle />
         <Provider store={store}>
-            <Header />
+            <div>
+                <Header />
+                <BrowserRouter>
+                    <div>
+                        <Route path='/' exact render={() => <div>home</div>}></Route>
+                        <Route path='/detail' render={() => <div>detail</div>}></Route>
+                    </div>
+                </BrowserRouter>
+            </div>
         </Provider>
     </div>
   );
